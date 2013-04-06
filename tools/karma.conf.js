c9ace8e7c59ba166b5d46da022b71987ca663d72
Javier Mendiara Cañardo <javier@tid.es>
2013-04-06 21:33:21 +0200

    ADD travis

--- tools/karma.conf.js
+++ tools/karma.conf.js
@@ -65,7 +65,7 @@ autoWatch = true;
 // - Safari (only Mac)
 // - PhantomJS
 // - IE (only Windows)
-browsers = ['Chrome'];
+browsers = ['PhantomJS'];
 
 
 // If browser does not capture in given timeout [ms], kill it
